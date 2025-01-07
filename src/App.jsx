c0c57676f3de9e95bd98bdfcb529211dc3fdace9
shohjahon-asqarov <shohjahonasqarov.web@gmail.com>
2025-01-07 16:39:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Abdulaziz from './components/Abdulaziz'
+
+const App = () => {
+
+
+  const [num, setNum] = useState(0)
+
+  const increment = () => {
+    setNum(num + 1)
+  }
+
+  return (
+    <>
+      <div className='flex justify-center space-x-4 mt-20'>
+        <span className='block text-center font-medium text-4xl'>{num}</span>
+        <button onClick={increment} className='py-2 px-5 bg-blue-600 text-white rounded-md'>+</button>
+      </div>
+      <Abdulaziz />
+    </>
+  )
+}
+
+export default App
